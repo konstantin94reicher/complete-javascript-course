d68fb5bf8bff79bbc691ee6fd5480f101a6a5b61
konstantin94reicher <205547482+konstantin94reicher@users.noreply.github.com>
2025-12-21 14:15:13 +0100

    finished challenge 2, updated challenge1.js

--- 00-codingChallenges/challenge1.js
+++ 00-codingChallenges/challenge1.js
@@ -1,3 +1,5 @@
+// Challenge 1
+
 // Mark and John are trying to compare their BMI (Body Mass Index), which is
 // calculated using the formula:
 // BMI = mass / height ** 2 = mass / (height * height) (mass in kg
@@ -30,11 +32,43 @@ heightJohn = 1.95;
 let bmiMark = calculateBMI(massMark, heightMark); 
 let bmiJohn = calculateBMI(massJohn, heightJohn); 
 
-let markHigherBMI = (bmi1, bmi2) => bmi1 > bmi2;
+let markHigherBMI = (bmi1, bmi2) => {
+    if (bmi1 > bmi2) {
+        return `Mark's BMI (${bmi1}) is higher than John's (${bmi2}).`; 
+    } else if (bmi1 < bmi2) {
+        return `John's BMI (${bmi2}) is higher than Mark's (${bmi1}).`;
+    } else {
+        return `both BMI's are equally high (${bmi1})`;
+    }
+};
 
-console.log(`BMI Mark Test 1: ${bmiMark}`);
-console.log(`BMI John Test 1: ${bmiJohn}`);
-alert (`BMI Mark higher than BMI John: ${markHigherBMI(bmiMark, bmiJohn)}`); 
+// console.log(`BMI Mark Test 1: ${bmiMark}`);
+// console.log(`BMI John Test 1: ${bmiJohn}`);
+// alert (`BMI Mark higher than BMI John: ${markHigherBMI(bmiMark, bmiJohn)}`); 
+
+// massMark = 95; 
+// heightMark = 1.88; 
+// massJohn = 85; 
+// heightJohn = 1.76; 
+
+// bmiMark = calculateBMI(massMark, heightMark); 
+// bmiJohn = calculateBMI(massJohn, heightJohn); 
+
+// console.log(`BMI Mark Test 2: ${bmiMark}`);
+// console.log(`BMI John Test 2: ${bmiJohn}`);
+// alert (`BMI Mark higher than BMI John: ${markHigherBMI(bmiMark, bmiJohn)}`); 
+
+// Challenge 2
+// Use the BMI example from Challenge #1, and the code you already wrote, and
+// improve it.
+// Your tasks:
+// 1. Print a nice output to the console, saying who has the higher BMI. The message
+// is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+// 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+// BMI (28.3) is higher than John's (23.9)!"
+// Hint: Use an if/else statement 😉
+
+alert (markHigherBMI(bmiMark, bmiJohn));
 
 massMark = 95; 
 heightMark = 1.88; 
@@ -44,6 +78,14 @@ heightJohn = 1.76;
 bmiMark = calculateBMI(massMark, heightMark); 
 bmiJohn = calculateBMI(massJohn, heightJohn); 
 
-console.log(`BMI Mark Test 2: ${bmiMark}`);
-console.log(`BMI John Test 2: ${bmiJohn}`);
-alert (`BMI Mark higher than BMI John: ${markHigherBMI(bmiMark, bmiJohn)}`); 
+alert (markHigherBMI(bmiMark, bmiJohn));
+
+massMark = 95; 
+heightMark = 1.88; 
+massJohn = 95; 
+heightJohn = 1.88; 
+
+bmiMark = calculateBMI(massMark, heightMark); 
+bmiJohn = calculateBMI(massJohn, heightJohn); 
+
+alert (markHigherBMI(bmiMark, bmiJohn));
